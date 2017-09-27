da1c3132d519150308ab130a27d35ed0111fea04
Anton Smirnov <dev@antonsmirnov.name>
2017-09-27 14:07:36 +0500

    Issue 5800 - Add option to specify V8 library

--- README.md
+++ README.md
@@ -91,6 +91,19 @@ An app that uses the library have to add the following permissions to `AndroidMa
 
 (added automatically if building with Gradle or should be added manually otherwise).
 
+### Building with prebuilt shared V8
+
+This can be desired to use product's V8 (let's say Chromium) instead of libadblockplus built-in V8.
+Put prebuilt shared V8 library file in ARCH directories and set `SHARED_V8_LIB_FILENAME`
+environment variable before building. Libadblockplus is required to be linked with that library file.
+
+For example (build with Gradle):
+
+    SHARED_V8_LIB_FILENAME=libswev8.so ./gradlew clean assemble
+    
+Note `[Configuration] Linking dynamically with shared v8 library ./libadblockplus-binaries/android_armeabi-v7a/libv8.cr.so`
+output while building native library. 
+
 ## Library tests
 
 Android tests for the library.
--- libadblockplus-android/jni/Android.mk
+++ libadblockplus-android/jni/Android.mk
@@ -1,5 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
+# Report configuration
+ifeq ($(SHARED_V8_LIB_FILENAME),)
+# static
+$(info [Configuration] Linking statically with built-in v8)
+else
+# dynamic
+$(info [Configuration] Linking dynamically with shared v8 library ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/$(SHARED_V8_LIB_FILENAME))
+endif
+
+# libadblockplus.a
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libadblockplus
@@ -7,27 +17,7 @@ LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libadblo
 
 include $(PREBUILT_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := v8-libsampler
-LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_libsampler.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := v8-base
-LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_base.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := v8-libbase
-LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_libbase.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
+# libv8-platform.a
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := v8-libplatform
@@ -35,6 +25,34 @@ LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_li
 
 include $(PREBUILT_STATIC_LIBRARY)
 
+ifeq ($(SHARED_V8_LIB_FILENAME),)
+# static
+
+# libv8-libsampler.a
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := v8-libsampler
+LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_libsampler.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# libv8-base.a
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := v8-base
+LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_base.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# libv8_libbase.a
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := v8-libbase
+LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_libbase.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# libv8_snapshot.a
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := v8-snapshot
@@ -42,11 +60,22 @@ LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/libv8_sn
 
 include $(PREBUILT_STATIC_LIBRARY)
 
+else
+# dynamic
+
+# prebuilt shared library v8
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libv8
+LOCAL_SRC_FILES := ./libadblockplus-binaries/android_$(TARGET_ARCH_ABI)/$(SHARED_V8_LIB_FILENAME)
+include $(PREBUILT_SHARED_LIBRARY)
+
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libadblockplus-jni
 LOCAL_SRC_FILES := JniLibrary.cpp
-LOCAL_SRC_FILES += JniPlatform.cpp
 LOCAL_SRC_FILES += JniJsEngine.cpp JniFilterEngine.cpp JniJsValue.cpp
 LOCAL_SRC_FILES += JniFilter.cpp JniSubscription.cpp JniEventCallback.cpp
 LOCAL_SRC_FILES += JniLogSystem.cpp JniWebRequest.cpp
@@ -55,10 +84,19 @@ LOCAL_SRC_FILES += JniFilterChangeCallback.cpp JniCallbacks.cpp Utils.cpp
 LOCAL_SRC_FILES += JniNotification.cpp JniShowNotificationCallback.cpp
 LOCAL_SRC_FILES += JniIsAllowedConnectionTypeCallback.cpp
 
-LOCAL_CPP_FEATURES := exceptions 
+LOCAL_CPP_FEATURES := exceptions
 LOCAL_CPPFLAGS += -std=c++11
 
 LOCAL_C_INCLUDES := jni/libadblockplus-binaries/include/
-LOCAL_STATIC_LIBRARIES := libadblockplus v8-base v8-snapshot v8-libsampler v8-libplatform v8-libbase
+
+LOCAL_STATIC_LIBRARIES := libadblockplus v8-libplatform
+
+ifeq ($(SHARED_V8_LIB_FILENAME),)
+# static
+LOCAL_STATIC_LIBRARIES += v8-base v8-snapshot v8-libsampler v8-libbase
+else
+# dynamic
+LOCAL_STATIC_LIBRARIES += libv8
+endif
 
 include $(BUILD_SHARED_LIBRARY)
